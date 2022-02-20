74046f4d46432be306b21689e4f04e3ed34d1db1
SafwanGanz <67726326+SafwanGanz@users.noreply.github.com>
2022-02-21 03:12:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const chalk = require('chalk');
+const {WAConnection, MessageType} = require('@adiwajshing/baileys');
+const fs = require('fs');
+
+async function whatsfelix () {
+    const conn = new WAConnection();
+    conn.logger.level = 'warn';
+
+    conn.on('connecting', async () => {
+        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Felix')}
+${chalk.white.italic('FelixString Code Recipient')}
+
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
+    });
+    
+
+    conn.on('open', async () => {
+        console.log(
+            chalk.green.bold('Felix QR Code: '), 
+            'FELIX;;;' + Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')
+        );
+        await conn.sendMessage(conn.user.jid, 'FELIX;;;' + Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64'), MessageType.text );
+        console.log(            
+            chalk.green.bold('IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!')
+        );
+        process.exit(0);
+    });
+
+    await conn.connect();
+}
+
+whatsfelix()
